ff77d416bf796c68f82991ac6452864e8a9acfba
d4h2nu8h <84399289+d4h2nu8h@users.noreply.github.com>
2026-03-15 01:12:30 -0400

    Add files via upload

---
+++ Backend/main_file.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import Flask, app, request
+import json
+from base64 import b64decode, b64encode
+from flask_cors import CORS, cross_origin
+import requests
+app = Flask(__name__)
+cors = CORS(app)
+ngrok = "https://d14c-35-204-187-78.ngrok-free.app"
+@app.route('/fake_review', methods=['GET','POST'])
+def a():
+    uri_data = request.json
+    print(uri_data)
+    x = [str(i) for i in uri_data.values()]
+    img = ','.join(x)
+    print(img, type(img))
+    r = requests.post(f"{ngrok}/fake_review", data={'uri':img})
+    
+    return r.json()
+    
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
